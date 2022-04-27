2ce6defa12af8b3a66db2d36b62de97ceedfe6bd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-04-27 12:26:05 +0300

    Create BasicStackOperations.java

---
+++ Stacks and Queues - Exercises/BasicStackOperations.java
@@ -0,0 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BasicStackOperations {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        ArrayDeque<Integer> number = new ArrayDeque<>();
+        int elementToPush = console.nextInt();
+        int elementToPop = console.nextInt();
+        int elementToCheck = console.nextInt();
+        for (int i = 0; i < elementToPush; i++) {
+            number.push(console.nextInt());
+        }
+        for (int i = 0; i < elementToPop; i++) {
+            number.pop();
+        }
+        if (number.size() == 0) System.out.println(0);
+        else if (number.contains(elementToCheck)) System.out.println("true");
+        else System.out.println(number.stream().min(Integer::compare).get());
+    }
+}
