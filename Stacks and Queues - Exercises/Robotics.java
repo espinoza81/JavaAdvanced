abf5b97d8369a739d21985fb4ee494c8248a18ae
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-11 09:32:29 +0300

    Update Robotics.java

--- Stacks and Queues - Exercises/Robotics.java
+++ Stacks and Queues - Exercises/Robotics.java
@@ -1,44 +1,44 @@
-
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Robotics {
-    static class Robot{
-        String name;
-        int processTime;
-        LocalTime endTime;
-
-        public String getName() {
-            return name;
-        }
-
-        public int getProcessTime() {
-            return processTime;
-        }
-
-        public LocalTime getEndTime() {
-            return endTime;
-        }
-
-        public Robot(String name, int processTime) {
-            this.name = name;
-            this.processTime = processTime;
-            this.endTime = LocalTime.of(0, 0, 0);
-        }
-    }
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        List<Robot> robots = new ArrayList<>();
+        HashMap<String, Integer> robotProcessTime = new LinkedHashMap<>();
+        HashMap<String, LocalTime> robotBusy = new LinkedHashMap<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         Arrays.stream(console.nextLine().split(";"))
                 .forEach(s -> {
                     String name = s.split("-")[0];
-                    int processTime = Integer.parseInt(s.split("-")[0]);
-                    Robot singleRobot = new Robot(name, processTime);
-                    robots.add(singleRobot);
+                    int processTime = Integer.parseInt(s.split("-")[1]);
+                    robotProcessTime.put(name, processTime);
+                    robotBusy.put(name, LocalTime.parse("00:00:00", formatter));
                 });
-        LocalTime starTime = LocalTime.parse(console.nextLine());
+        String time = console.nextLine();
+        if(time.length()==7) time = "0" + time;
+        LocalTime starTime = LocalTime.parse(time, formatter);
+        ArrayDeque<String> products = new ArrayDeque<>();
         String product;
-        while (!"End".equals(product = console.nextLine())){
+        while (!"End".equals(product = console.nextLine())) {
+            starTime = starTime.plusSeconds(1);
+            String tempRobot = robotBusy.entrySet().stream().min(Map.Entry.comparingByValue()).stream().findFirst().get().getKey();
+            if (robotBusy.get(tempRobot).compareTo(starTime) <= 0) {
+                robotBusy.put(tempRobot, starTime.plusSeconds(robotProcessTime.get(tempRobot)));
+                System.out.println(tempRobot + " - " + product + " [" + starTime.format(formatter) + "]");
+            } else {
+                products.offer(product);
+            }
+        }
+        while (!products.isEmpty()) {
+            starTime = starTime.plusSeconds(1);
+            String tempRobot = robotBusy.entrySet().stream().min(Map.Entry.comparingByValue()).stream().findFirst().get().getKey();
+            if (robotBusy.get(tempRobot).compareTo(starTime) <= 0) {
+                robotBusy.put(tempRobot, starTime.plusSeconds(robotProcessTime.get(tempRobot)));
+                System.out.println(tempRobot + " - " + products.poll() + " [" + starTime.format(formatter) + "]");
+            } else {
+                products.offer(products.poll());
+            }
         }
     }
 }
