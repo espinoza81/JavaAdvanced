1134d689e00ab8ba1901e64adb0cb14b0bc0ae8b
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-12 10:12:39 +0300

    Update Robotics.java

--- Stacks and Queues - Exercises/Robotics.java
+++ Stacks and Queues - Exercises/Robotics.java
@@ -1,4 +1,5 @@
-import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
@@ -6,38 +7,46 @@ public class Robotics {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         HashMap<String, Integer> robotProcessTime = new LinkedHashMap<>();
-        HashMap<String, LocalTime> robotBusy = new LinkedHashMap<>();
+        HashMap<String, LocalDateTime> robotBusy = new LinkedHashMap<>();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
         Arrays.stream(console.nextLine().split(";"))
                 .forEach(s -> {
                     String name = s.split("-")[0];
                     int processTime = Integer.parseInt(s.split("-")[1]);
                     robotProcessTime.put(name, processTime);
-                    robotBusy.put(name, LocalTime.parse("00:00:00", formatter));
+                    robotBusy.put(name, LocalDateTime.parse(LocalDate.now() + "T00:00:00"));
                 });
         String time = console.nextLine();
-        if(time.length()==7) time = "0" + time;
-        LocalTime starTime = LocalTime.parse(time, formatter);
+        if(time.length()==7) {
+            time = LocalDate.now() + "T0" + time;
+        }
+        else {
+            time = LocalDate.now() + "T" + time;
+        }
+        LocalDateTime starTime = LocalDateTime.parse(time);
         ArrayDeque<String> products = new ArrayDeque<>();
         String product;
         while (!"End".equals(product = console.nextLine())) {
             starTime = starTime.plusSeconds(1);
-            String tempRobot = robotBusy.entrySet().stream().min(Map.Entry.comparingByValue()).stream().findFirst().get().getKey();
-            if (robotBusy.get(tempRobot).compareTo(starTime) <= 0) {
-                robotBusy.put(tempRobot, starTime.plusSeconds(robotProcessTime.get(tempRobot)));
-                System.out.println(tempRobot + " - " + product + " [" + starTime.format(formatter) + "]");
+            LocalDateTime finalStarTime = starTime;
+            Optional<Map.Entry<String, LocalDateTime>> tempRobot = robotBusy.entrySet().stream().filter(s -> s.getValue().compareTo(finalStarTime) <= 0).findFirst();
+            if (tempRobot.isPresent()) {
+                robotBusy.put(tempRobot.get().getKey(), starTime.plusSeconds(robotProcessTime.get(tempRobot.get().getKey())));
+                System.out.println(tempRobot.get().getKey() + " - " + product + " [" + starTime.format(formatter) + "]");
             } else {
                 products.offer(product);
             }
         }
         while (!products.isEmpty()) {
+            product = products.poll();
             starTime = starTime.plusSeconds(1);
-            String tempRobot = robotBusy.entrySet().stream().min(Map.Entry.comparingByValue()).stream().findFirst().get().getKey();
-            if (robotBusy.get(tempRobot).compareTo(starTime) <= 0) {
-                robotBusy.put(tempRobot, starTime.plusSeconds(robotProcessTime.get(tempRobot)));
-                System.out.println(tempRobot + " - " + products.poll() + " [" + starTime.format(formatter) + "]");
+            LocalDateTime finalStarTime = starTime;
+            Optional<Map.Entry<String, LocalDateTime>> tempRobot = robotBusy.entrySet().stream().filter(s -> s.getValue().compareTo(finalStarTime) <= 0).findFirst();
+            if (tempRobot.isPresent()) {
+                robotBusy.put(tempRobot.get().getKey(), starTime.plusSeconds(robotProcessTime.get(tempRobot.get().getKey())));
+                System.out.println(tempRobot.get().getKey() + " - " + product + " [" + starTime.format(formatter) + "]");
             } else {
-                products.offer(products.poll());
+                products.offer(product);
             }
         }
     }
