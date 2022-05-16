773430ec38bf24224f6e51dfc2f2de4b31233ffc
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-16 13:23:43 +0300

    Update MatrixShuffling.java

--- Multidimensional Arrays - Exercises/MatrixShuffling.java
+++ Multidimensional Arrays - Exercises/MatrixShuffling.java
@@ -19,20 +19,21 @@ public class MatrixShuffling {
         while (!"END".equals(command = console.nextLine())) {
             Matcher matcher = pattern.matcher(command);
             if (matcher.find()) {
-
                 int row1 = Integer.parseInt(matcher.group("row1"));
                 int row2 = Integer.parseInt(matcher.group("row2"));
                 int col1 = Integer.parseInt(matcher.group("col1"));
                 int col2 = Integer.parseInt(matcher.group("col2"));
-                if (row1 < 0 || row1 >= row || row2 < 0 || row2 >= row || col1 < 0 || col1 >= col || col2 < 0 || col2 >= col) {
-                    System.out.println("Invalid input!");
-                } else {
-                    String temp;
+                String temp;
+
+                try {
                     temp = matrix[row1][col1];
                     matrix[row1][col1] = matrix[row2][col2];
                     matrix[row2][col2] = temp;
                     System.out.println(Arrays.deepToString(matrix).replace("], ", "\n").replaceAll("[\\[\\],]", ""));
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid input!");
                 }
+
             } else {
                 System.out.println("Invalid input!");
             }
