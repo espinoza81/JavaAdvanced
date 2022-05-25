c8c3d6283dc3668673c4664dda19c2c86863ff42
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-25 09:41:24 +0300

    Create 03PeriodicTable.java

---
+++ Sets And Maps Advanced - Exercises/03PeriodicTable.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class PeriodicTable {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int lineToInput = Integer.parseInt(console.nextLine());
+        Set<String> chemicalElements = new TreeSet<>();
+        for (int i=0; i< lineToInput; i++){
+            String[] elements = console.nextLine().split("\\s+");
+            chemicalElements.addAll(Arrays.asList(elements));
+        }
+        System.out.println(chemicalElements.toString().replaceAll("[\\[\\],]", ""));
+    }
+}
