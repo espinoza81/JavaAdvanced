1f5d7216466159a2c131ac625bb5f6629cc6b67f
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-26 10:19:33 +0300

    Create 13DragonArmy.java

---
+++ 06 Sets And Maps Advanced - Exercises/13DragonArmy.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DragonArmy {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        Map<String, HashMap<String,int[]>> dragons = new LinkedHashMap<>();
+        Map<String, double[]> dragonsStatsByType = new HashMap<>();
+        int number = Integer.parseInt(console.nextLine());
+        for(int i=0; i<number; i++){
+            String[] command = console.nextLine().split("\\s+");
+            //{type} {name} {damage} {health} {armor}
+            String type = command[0];
+            String name = command[1];
+            int damage = "null".equals(command[2])? 45 : Integer.parseInt(command[2]);
+            int health = "null".equals(command[3])? 250 : Integer.parseInt(command[3]);
+            int armor = "null".equals(command[4])? 10 : Integer.parseInt(command[4]);
+            int[] stats = new int[]{damage, health, armor};
+            dragons.putIfAbsent(type, new LinkedHashMap<>());
+            dragons.get(type).put(name, stats);
+        }
+        
+        dragons.forEach((key, value) -> {
+            double damage = 0.00;
+            double health = 0.00;
+            double armor = 0.00;
+            double count = 0.00;
+            for (int[] stats : value.values()) {
+                damage += stats[0];
+                health += stats[1];
+                armor += stats[2];
+                count++;
+            }
+            double[] statsAll = new double[]{damage, health, armor, count};
+            dragonsStatsByType.put(key, statsAll);
+        });
+        
+        dragons.forEach((key, value) -> {
+            double[] allStats = dragonsStatsByType.get(key);
+            double damage = allStats[0]/allStats[3];
+            double health = allStats[1]/allStats[3];
+            double armor = allStats[2]/allStats[3];
+            System.out.printf("%s::(%.2f/%.2f/%.2f)%n", key, damage, health, armor);
+            value.entrySet().stream()
+                    .sorted(Map.Entry.comparingByKey())
+                    .forEach(s ->
+                            System.out.printf("-%s -> damage: %d, health: %d, armor: %d%n", s.getKey(), s.getValue()[0], s.getValue()[1], s.getValue()[2]));
+        });
+    }
+}
