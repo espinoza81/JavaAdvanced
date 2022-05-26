2fcd7cd904682d68d32057610ba051e57c94068e
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-26 17:00:51 +0300

    Create 02WriteToFile.java

---
+++ 07 Streams, Files And Directories - Lab/02WriteToFile.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class WriteToFile {
+    public static void main(String[] args) throws IOException {
+        String inPath = "D:\\bobi\\Java\\Advanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String outputPath = "D:\\bobi\\Java\\Advanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\02.WriteToFileOutput.txt";
+        Set<Character> punctuations = new HashSet<>();
+        punctuations.add(',');
+        punctuations.add('.');
+        punctuations.add('?');
+        punctuations.add('!');
+        OutputStream out = new FileOutputStream(outputPath);
+        try (InputStream in = new FileInputStream(inPath)) {
+            int oneByte;
+            while ((oneByte = in.read()) >= 0) {
+                if(!punctuations.contains((char) oneByte)){
+                    out.write(oneByte);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
