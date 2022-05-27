98b595d451b2b1a45120d9f7c777acde2bb5af0c
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-27 08:34:45 +0300

    Update 02WriteToFile.java

--- 07 Streams, Files And Directories - Lab/02WriteToFile.java
+++ 07 Streams, Files And Directories - Lab/02WriteToFile.java
@@ -8,10 +8,7 @@ public class WriteToFile {
         String inPath = "D:\\bobi\\Java\\Advanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
         String outputPath = "D:\\bobi\\Java\\Advanced\\src\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\02.WriteToFileOutput.txt";
         Set<Character> punctuations = new HashSet<>();
-        punctuations.add(',');
-        punctuations.add('.');
-        punctuations.add('?');
-        punctuations.add('!');
+        Collections.addAll(punctuations, '.', ',', '!', '?');
         OutputStream out = new FileOutputStream(outputPath);
         try (InputStream in = new FileInputStream(inPath)) {
             int oneByte;
