d5f45ff2a3c34d915c60affd8afb030319c98e0f
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-05-31 16:42:47 +0300

    Create Car.java

---
+++ 11 Defining Classes - Lab/01 Car Info/Car.java
@@ -0,0 +1,35 @@
+package CarInfo;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public String carInfo() {
+        return String.format("The car is: %s %s - %d HP.", this.getBrand(), this.getModel(), this.getHorsePower());
+    }
+}
