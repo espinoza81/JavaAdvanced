aff06639cc8ab4b69f8347494009cf9e4dc1da76
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-01 14:52:54 +0300

    Create Person.java

---
+++ 12 Defining Classes - Exercises/01 Opinion Poll/Person.java
@@ -0,0 +1,24 @@
+package OpinionPoll;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age;
+    }
+}
