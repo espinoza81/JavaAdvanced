aba30df47482f50b12a647255a7917d88ee029dd
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-02 10:17:31 +0300

    Create Engine.java

---
+++ 12 Defining Classes - Exercises/05 Car Salesman/Engine.java
@@ -0,0 +1,52 @@
+package CarSalesman;
+
+public class Engine {
+    private String model;
+    public int power;
+    private int displacement = 0;
+    private String efficiency = "n/a";
+
+    public Engine(String model, int power, int displacement, String efficiency) {
+        this.model = model;
+        this.power = power;
+        this.displacement = displacement;
+        this.efficiency = efficiency;
+    }
+
+    public Engine(String model, int power) {
+        this.model = model;
+        this.power = power;
+    }
+
+    public Engine(String model, int power, int displacement) {
+        this.model = model;
+        this.power = power;
+        this.displacement = displacement;
+    }
+
+    public Engine(String model, int power, String efficiency) {
+        this.model = model;
+        this.power = power;
+        this.efficiency = efficiency;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    public String getEfficiency() {
+        return efficiency;
+    }
+}
