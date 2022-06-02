5e73d2eef5ffc5e18d693fc624e4127a8ddd512b
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-02 13:27:10 +0300

    Create Children.java

---
+++ 12 Defining Classes - Exercises/07 Google/Children.java
@@ -0,0 +1,31 @@
+package Google;
+
+public class Children {
+    private String name;
+    private String birthday;
+
+    public Children(String name, String birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.birthday;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+}
