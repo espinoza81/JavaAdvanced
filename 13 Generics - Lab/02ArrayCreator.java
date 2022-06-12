0782af1fed2e1ef57932b6d034cdb80cd5195eda
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-12 18:24:28 +0300

    Update 02ArrayCreator.java

--- 13 Generics - Lab/02ArrayCreator.java
+++ 13 Generics - Lab/02ArrayCreator.java
@@ -4,12 +4,15 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class ArrayCreator {
+    
+    @SuppressWarnings("unchecked")
     public static <T> T[] create(int length, T item){
         T[] arr = (T[]) Array.newInstance(item.getClass(), length);
         Arrays.fill(arr, item);
         return arr;
     }
 
+    @SuppressWarnings("unchecked")
     public static <T> T[] create(Class<T> clazz, int length, T item){
         T[] arr = (T[]) Array.newInstance(clazz, length);
         Arrays.fill(arr, item);
