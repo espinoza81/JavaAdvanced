e83ce8a51174279d242d8a9e0c1152d9170aea55
Bonka Tsvetanova <espinoza81@yahoo.com>
2022-06-16 09:50:53 +0300

    Create ListyIterator.java

---
+++ 16 Iterators and Comparators - Exercises/01. ListyIterator/ListyIterator.java
@@ -0,0 +1,35 @@
+package Iterators;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListyIterator {
+    private List<String> collection;
+    int index;
+
+    public ListyIterator(String... input) {
+        this.collection = Arrays.asList(input);
+        this.index=0;
+    }
+
+    public boolean Move() {
+        if (hasNext()) {
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasNext() {
+        return index < collection.size()-1;
+    }
+
+    public void Print() {
+        if(collection.isEmpty()){
+            System.out.println("Invalid Operation!");
+        }
+        else {
+            System.out.println(collection.get(index));
+        }
+    }
+}
